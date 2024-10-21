798b1c59b38b797b067be3335d217e7c6d2e3fe0
pinguinos <leina08@gmail.com>
2024-10-20 21:16:44 -0400

    day 3 part 2

--- 2023/day3part2.py
+++ 2023/day3part2.py
@@ -2,6 +2,7 @@ import operator
 import sys
 
 from argparse import ArgumentParser
+from collections import defaultdict
 from functools import reduce
 
 def main(argv):
@@ -10,7 +11,7 @@ def main(argv):
     f = open(args.inputFilePath, 'r')
 
     inputs = [line for line in f] # not stripping, using end of line "\n" as a buffer for symbol checking
-    nums = []
+    starMap = defaultdict(list)
 
     for rindx, row in enumerate(inputs):
         numstr = ""
@@ -22,18 +23,28 @@ def main(argv):
                     pstart = cindx 
             else:
                 if pstart != -1:
-                    if has_surround_symbol(rindx ,pstart, numstr, inputs):
-                        nums.append( int(numstr.strip()) )
+                    check_star_symbol(rindx ,pstart, numstr, inputs, starMap)
 
                 pstart = -1
                 numstr = ""
 
-    result = reduce(operator.add, nums)
+    result = compute_gear_ratio(starMap)
     print("Result = {}".format(result))
     return
 
 
-def has_surround_symbol(rindx, pstart, numstr, inputs):
+def compute_gear_ratio(starMap):
+    
+    sumGratio = 0
+    for v in starMap.values():
+        if len(v) == 2:
+            gratio = reduce(operator.mul, v)
+            sumGratio += gratio
+
+    return sumGratio
+        
+
+def check_star_symbol(rindx, pstart, numstr, inputs, starMap):
 
     for r in range(rindx-1, (rindx+1)+1): # +1 because range is not inclusive at end bound
         if r < 0 or r >= len(inputs):
@@ -41,11 +52,10 @@ def has_surround_symbol(rindx, pstart, numstr, inputs):
         for c in range(pstart-1, ( pstart+len(numstr) ) + 1): 
             if c < 0 or c >= len(inputs[rindx]):
                 continue
-            if not inputs[r][c].isdigit() and inputs[r][c] != "." and inputs[r][c] != "\n":
-                return True
-    return False
+            if inputs[r][c] == "*":
+                starMap[(r, c)].append(int(numstr))
+    return
     
-
 
 
 def parse_arg(argv):
